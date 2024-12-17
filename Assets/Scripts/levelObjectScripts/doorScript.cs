9d6096c786bf58993969c92978ac5a5b280cc5d1
Jino Tesauro <jinotesauro@pop-os.localdomain>
2024-12-16 19:55:48 -0600

    Got final dog level

--- Assets/Scripts/levelObjectScripts/doorScript.cs
+++ Assets/Scripts/levelObjectScripts/doorScript.cs
@@ -12,17 +12,9 @@ public class DoorScript : MonoBehaviour
     //Gets called when something collides with the door. This is where we can add scene change logic.
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
-            GameObject.Find("Door").GetComponent<SpriteRenderer>().color = new Color(0, 0, 255);
 
             Debug.Log("Test");
             SceneManager.LoadScene(nextScene);        
-        }
-    }
-
-    //Gets called when collider leaves door. We may need this for animation or scene change logic.
-    private void OnTriggerExit2D(Collider2D other) {
-        if (other.gameObject.CompareTag("Player")) {
-            GameObject.Find("Door").GetComponent<SpriteRenderer>().color = new Color(255, 0, 0);
         }
     }
         
